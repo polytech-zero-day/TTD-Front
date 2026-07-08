d62e22d4a8b8c2f7ac30d336d061f3a8bf39768b
chayh414 <chayh414@gmail.com>
2026-07-08 18:04:23 +0900

    [SCRUM-20] feat: 리더보드 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AuthPage from './pages/AuthPage';
 import AdminLayout from './pages/admin/AdminLayout';
 import UserManagementPage from './pages/admin/UserManagementPage';
 import MyPage from './pages/MyPage';
+import Leaderboard from './pages/Leaderboard';
 
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/login" element={<AuthPage mode="login" />} />
         <Route path="/signup" element={<AuthPage mode="signup" />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<Navigate to="users" replace />} />
           <Route path="users" element={<UserManagementPage />} />
